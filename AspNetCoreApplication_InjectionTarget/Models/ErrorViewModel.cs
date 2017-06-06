84aeae2d6a56bc5cef2f18919df8c0110b965834
Gergely Kalapos <gergo@kalapos.net>
2017-06-06 13:23:19 +0200

    Add project files.

---
+++ AspNetCoreApplication_InjectionTarget/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreApplication_InjectionTarget.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AwesomeHostingStartup/AwesomeHostingStartup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using AwesomeHostingStartupLib;
+using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(AwesomeHostingStartup))]
+
+namespace AwesomeHostingStartupLib
+{
+    internal class Program { public static void Main() { } }
+
+    public class AwesomeHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            Console.WriteLine("==========================MY_AWESOME_HOSTINGSTARTUP_LOADED======================");
+
+            var _diagnosticsListenersSubscription =
+                DiagnosticListener.AllListeners.Subscribe(new DummyObserver<DiagnosticListener>((listener)
+                => {
+                    var _handledExceptionSubscription =
+                       listener.Subscribe(new DummyObserver<KeyValuePair<string, object>>((diagnosticEvent) => {
+                           Console.WriteLine($"DiagnosticSource Event: {diagnosticEvent.Key}");
+                       }));
+                }));
+        }
+
+        private class DummyObserver<T> : IObserver<T>
+        {
+            public DummyObserver(Action<T> callback) { _callback = callback; }
+            public void OnCompleted() { }
+            public void OnError(Exception error) { }
+            public void OnNext(T value) { _callback(value); }
+
+            private Action<T> _callback;
+        }
+    }
+}
